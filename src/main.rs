3fc41bc4ce721fdf0104392a8f1f0eaa07cd8846
Bruno Paulino <bruno@bpaulino.com>
2023-02-11 23:56:40 +0100

    Using the Future trait

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use futures::{
+    future::{BoxFuture, FutureExt},
+    task::{waker_ref, ArcWake},
+};
+
+use std::{
+    future::Future,
+    sync::mpsc::{sync_channel, Receiver, SyncSender},
+    sync::{Arc, Mutex},
+    task::Context,
+    time::Duration,
+};
+
+use async_rust::TimerFuture;
+
+fn main() {}
