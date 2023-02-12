855f910d3297377560849f03f6bb28b4afcbd4c4
Bruno Paulino <bruno@bpaulino.com>
2023-02-12 16:47:57 +0100

    Implement Executor based on channels

--- src/lib.rs
+++ src/lib.rs
@@ -22,8 +22,14 @@ impl Future for TimerFuture {
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let mut shared_state = self.shared_state.lock().unwrap();
         if shared_state.completed {
+            // This future is completed. No need to reference the waker anymore.
+            // This future should be removed from the executor queue.
             Poll::Ready(())
         } else {
+            // The concrete Waker comes from the execution context when Poll-ing it
+            // So we store it here in order to call it once we are ready to make progress.
+            // We always need to clone it so we keep the up2date reference from this context,
+            // otherwise different threads could have a stale pointers to the waker.
             shared_state.waker = Some(cx.waker().clone());
             Poll::Pending
         }
@@ -37,9 +43,13 @@ impl TimerFuture {
             waker: None,
         }));
 
+        // Make a copy of the shared state pointer so we can move
+        // its reference to the thread scope which will own it.
         let thread_shared_state = shared_state.clone();
         thread::spawn(move || {
+            println!("🏋🏽 Doing something heavy on a thread...");
             thread::sleep(duration);
+            println!("✅ Done doing something heavy. took {:?}", duration);
             let mut shared_state = thread_shared_state.lock().unwrap();
             shared_state.completed = true;
             if let Some(waker) = shared_state.waker.take() {
--- src/main.rs
+++ src/main.rs
@@ -13,4 +13,91 @@ use std::{
 
 use async_rust::TimerFuture;
 
-fn main() {}
+/// Task is a type of future that can reschedule itself in the queue
+/// until completion, so it can be polled by the `Executor`.
+struct Task {
+    /// In-flight future that should be pushed to completion
+    ///
+    /// Even though we are using just one thread o interleave tasks
+    /// a Mutex is necessary here so Rust knows that we can't mutate
+    /// this value unsafely across threads without acquiring the lock.
+    /// According to the async Rust book, in a production-grade Executor we
+    /// would use `UnsafeCell` instead.
+    future: Mutex<Option<BoxFuture<'static, ()>>>,
+    /// mechanism to reschedule itself back onto the task queue.
+    task_sender: SyncSender<Arc<Task>>,
+}
+
+impl ArcWake for Task {
+    fn wake_by_ref(arc_self: &Arc<Self>) {
+        // send itself back to the task channel so it will be picked up
+        // by the Executor when polled again.
+        let cloned = arc_self.clone();
+        arc_self
+            .task_sender
+            .send(cloned)
+            .expect("Too many tasks queued");
+    }
+}
+
+/// Executor that receives tasks from a channel and runs them.
+struct Executor {
+    ready_queue: Receiver<Arc<Task>>,
+}
+
+impl Executor {
+    fn run(&self) {
+        while let Ok(task) = self.ready_queue.recv() {
+            // take a future from the channel inbox and while it's not yet completed,
+            // poll it and attempt to complete. If it's still pending, put it back.
+            let mut maybe_future = task.future.lock().unwrap();
+            if let Some(mut future) = maybe_future.take() {
+                // create a local scope Waker from the task itself
+                // so we can extract the the future's context to poll it.
+                let waker = waker_ref(&task);
+                let context = &mut Context::from_waker(&waker);
+                if future.as_mut().poll(context).is_pending() {
+                    // Future isn't done it. put it back so it can be poll again
+                    // at a later point by the executor.
+                    *maybe_future = Some(future);
+                }
+            }
+        }
+    }
+}
+
+/// Spawns new futures onto the task channel
+struct Spawner {
+    task_sender: SyncSender<Arc<Task>>,
+}
+
+impl Spawner {
+    fn spawn(&self, future: impl Future<Output = ()> + 'static + Send) {
+        let future = future.boxed();
+        let task = Arc::new(Task {
+            future: Mutex::new(Some(future)),
+            task_sender: self.task_sender.clone(),
+        });
+        self.task_sender.send(task).expect("Too many tasks queued");
+    }
+}
+
+fn new_executor_and_spanwer() -> (Executor, Spawner) {
+    const MAX_QUEUED_TASKS: usize = 10_000;
+    let (task_sender, ready_queue) = sync_channel(MAX_QUEUED_TASKS);
+    (Executor { ready_queue }, Spawner { task_sender })
+}
+
+fn main() {
+    let (executor, spanwer) = new_executor_and_spanwer();
+
+    spanwer.spawn(async {
+        println!("Kicking task off...");
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("TimerFuture done!");
+    });
+
+    drop(spanwer);
+
+    executor.run();
+}
